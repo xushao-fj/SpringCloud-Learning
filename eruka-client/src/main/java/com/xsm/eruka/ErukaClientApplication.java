a1db7e514884b789bee9764a1476c808b14b44bd
xushao-fj <xushaomin_it@163.com>
2019-10-13 00:14:04 +0800

    feat: eruka demo init

---
+++ eruka-client/src/main/java/com/xsm/eruka/ErukaClientApplication.java
@@ -0,0 +1,13 @@
+package com.xsm.eruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErukaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErukaClientApplication.class, args);
+    }
+
+}
